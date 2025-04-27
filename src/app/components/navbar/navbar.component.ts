e708d4b6d31909b01ee7fe370c33009374e241c6
Marius Bogdanel <marius.bogdanel@gmail.com>
2025-04-27 21:05:51 +0100

    nav and contact changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  host: {
+    '[class.contact-page]': 'isContactPage',
+  },
 })
 export class NavbarComponent implements OnInit {
   active = 1;
+  isContactPage = false;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isContactPage = event.url === '/contact';
+      }
+    });
+  }
 
   ngOnInit(): void {}
 
